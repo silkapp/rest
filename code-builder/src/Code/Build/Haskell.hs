7e8b7aea13942a540dde3ae48a9fd6255072c954
Bram Schuur <bram@typlab.com>
2012-03-19 11:22:07 +0100

    First working version of Haskell bindings. Refactored out view datatypes from api.

---
+++ code-builder/src/Code/Build/Haskell.hs
@@ -0,0 +1,45 @@
+module Code.Build.Haskell where
+
+import Code.Build
+import Data.List
+
+record :: [(String, Code)] -> Code
+record []    = code "{}"
+record items = ("{ " <-> many ", ") |><| mkStack (map fst items) |><| many " = " |>+<| map snd items <-> "}"
+
+block :: CodeList a => a -> Code
+block = mkStack . codeList
+
+hsModule :: CodeList a => String -> a -> Code
+hsModule name c = "module" <++> name <++> "where" <-> block c
+
+hsType :: [String] -> String
+hsType = intercalate " -> "
+
+hsDecl :: Codeable a => String -> [String] -> a -> Code
+hsDecl n pars c | singleLine (code c) = n <++> intercalate " " pars <++> "= " <+| c
+                | otherwise = n <++> intercalate " " pars <++> "=" <-> indent 2 c
+
+function :: String -> String -> Code
+function n t = n <++> "::" <++> t
+
+hsLet :: (CodeList a, Codeable b) => a -> b -> Code
+hsLet decls f = ("let " <+| block decls) <-> "in " <+| code f
+
+hsArray :: CodeList a => a -> Code
+hsArray = surround "[" "]" . interleave ", "
+
+hsTuple :: CodeList a => a -> Code
+hsTuple = parenthesis . interleave ", "
+
+hsData :: String -> [String] -> Code
+hsData n cons = "data" <++> n <+| (" = " <-> many " | ") |><| block cons
+
+string :: Codeable a => a -> Code
+string = surround "\"" "\""
+
+infix 2 .=.
+
+(.=.) :: Codeable b => String -> b -> Code
+v .=. c | singleLine (code c) = v <+> " = " <+| c
+        | otherwise           = v <+> " =" <-> indent 2 c
