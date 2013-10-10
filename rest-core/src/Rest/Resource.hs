ca4320429503750520987d17490383d371a3ee74
Erik Hesselink <erik@typlab.com>
2013-10-10 12:24:45 +0200

    Rest-core: indentation.

--- rest-core/src/Rest/Resource.hs
+++ rest-core/src/Rest/Resource.hs
@@ -29,22 +29,22 @@ newtype Void = Void { magic :: forall a. a }
 
 data Resource m s sid mid aid where
   Resource :: (Monad m, Monad s) =>
-    { name           :: String                      -- ^ The name for this resource, used as a path segment in routing.
-    , description    :: String                      -- ^ A description of the resource, used for documentation.
-    , schema         :: Schema sid mid aid          -- ^ The schema for routing and identification.
-    , private        :: Bool                        -- ^ Private resources are not documented, but they are exposed.
-    , enter          :: forall b. sid -> s b -> m b -- ^ How to run a subresource given an id.
+    { name        :: String                      -- ^ The name for this resource, used as a path segment in routing.
+    , description :: String                      -- ^ A description of the resource, used for documentation.
+    , schema      :: Schema sid mid aid          -- ^ The schema for routing and identification.
+    , private     :: Bool                        -- ^ Private resources are not documented, but they are exposed.
+    , enter       :: forall b. sid -> s b -> m b -- ^ How to run a subresource given an id.
 
-    , list           :: mid -> ListHandler m        -- ^ List handler, both toplevel and deeper (search).
+    , list        :: mid -> ListHandler m        -- ^ List handler, both toplevel and deeper (search).
 
-    , statics        :: aid -> Handler m            -- ^ Static actions, e.g. signin.
+    , statics     :: aid -> Handler m            -- ^ Static actions, e.g. signin.
 
-    , get            :: Maybe (Handler s)           -- ^ Get a single resource identified by id.
-    , update         :: Maybe (Handler s)           -- ^ Update a single resource identified by id.
-    , remove         :: Maybe (Handler s)           -- ^ Delete a single resource identified by id.
-    , create         :: Maybe (Handler m)           -- ^ Create a single resource, generating a new id.
-    , actions        :: [(String, Handler s)]       -- ^ Actions performed on a single resource.
-    , selects        :: [(String, Handler s)]       -- ^ Properties of a single resource.
+    , get         :: Maybe (Handler s)           -- ^ Get a single resource identified by id.
+    , update      :: Maybe (Handler s)           -- ^ Update a single resource identified by id.
+    , remove      :: Maybe (Handler s)           -- ^ Delete a single resource identified by id.
+    , create      :: Maybe (Handler m)           -- ^ Create a single resource, generating a new id.
+    , actions     :: [(String, Handler s)]       -- ^ Actions performed on a single resource.
+    , selects     :: [(String, Handler s)]       -- ^ Properties of a single resource.
     } -> Resource m s sid mid aid
 
 mkResource :: (Monad m, Monad s) => Resource m s sid Void Void
