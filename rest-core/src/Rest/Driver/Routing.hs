dba5287c58d75e5cb3785725e260e4219d49f8f8
Erik Hesselink <erik@typlab.com>
2013-10-31 12:42:31 +0100

    Rest-core: make lines shorter.

--- rest-core/src/Rest/Driver/Routing.hs
+++ rest-core/src/Rest/Driver/Routing.hs
@@ -42,11 +42,22 @@ type UriParts = [String]
 apiError :: (MonadError (Reason e) m) => Reason e -> m a
 apiError = throwError
 
-newtype Router a = Router { unRouter :: ReaderT Method (StateT UriParts (EitherT Reason_ Identity)) a }
-  deriving (Functor, Applicative, Monad, MonadReader Method, MonadState UriParts, MonadError Reason_)
+newtype Router a =
+  Router { unRouter :: ReaderT Method (StateT UriParts (EitherT Reason_ Identity)) a }
+  deriving ( Functor
+           , Applicative
+           , Monad
+           , MonadReader Method
+           , MonadState UriParts
+           , MonadError Reason_
+           )
 
 runRouter :: Method -> UriParts -> Router (RunnableHandler m) -> Either Reason_ (RunnableHandler m)
-runRouter method uri router = runIdentity . runEitherT $ evalStateT (runReaderT (unRouter router) method) uri
+runRouter method uri = runIdentity
+                     . runEitherT
+                     . flip evalStateT uri
+                     . flip runReaderT method
+                     . unRouter
 
 route :: Method -> UriParts -> Rest.Api m -> Either Reason_ (RunnableHandler m)
 route method uri api = runRouter method uri $
@@ -69,17 +80,27 @@ routeRouter (Rest.Embed resource@(Rest.Resource { Rest.schema }) subRouters) =
       <|> lift (routeStep resource subRouters step)
        )
 
-routeToplevel :: Rest.Resource m s sid mid aid -> [Some1 (Rest.Router s)] -> Maybe (Rest.Cardinality sid mid) -> MaybeT Router (RunnableHandler m)
-routeToplevel resource@(Rest.Resource { Rest.list }) subRouters mToplevel = hoistMaybe mToplevel >>= \toplevel ->
+routeToplevel :: Rest.Resource m s sid mid aid
+              -> [Some1 (Rest.Router s)]
+              -> Maybe (Rest.Cardinality sid mid)
+              -> MaybeT Router (RunnableHandler m)
+routeToplevel resource@(Rest.Resource { Rest.list }) subRouters mToplevel =
+  hoistMaybe mToplevel >>= \toplevel ->
   case toplevel of
     Rest.Single sid -> lift $ withSubresource sid resource subRouters
-    Rest.Many   mid -> guardNullPath >> guardMethod GET >> return (RunnableHandler id (mkListHandler (list mid)))
+    Rest.Many   mid ->
+      do guardNullPath
+         guardMethod GET
+         return (RunnableHandler id (mkListHandler (list mid)))
 
 routeCreate :: Rest.Resource m s sid mid aid -> MaybeT Router (RunnableHandler m)
 routeCreate (Rest.Resource { Rest.create }) = guardNullPath >> guardMethod POST >>
   maybe (apiError UnsupportedRoute) (return . RunnableHandler id) create
 
-routeStep :: Rest.Resource m s sid mid aid -> [Some1 (Rest.Router s)] -> Rest.Step sid mid aid -> Router (RunnableHandler m)
+routeStep :: Rest.Resource m s sid mid aid
+          -> [Some1 (Rest.Router s)]
+          -> Rest.Step sid mid aid
+          -> Router (RunnableHandler m)
 routeStep resource subRouters step =
   case step of
     Rest.Named ns -> popSegment >>= \seg ->
@@ -88,24 +109,37 @@ routeStep resource subRouters step =
         Just h  -> routeNamed resource subRouters h
     Rest.Unnamed h -> routeUnnamed resource subRouters h
 
-routeNamed :: Rest.Resource m s sid mid aid -> [Some1 (Rest.Router s)] -> Either aid (Rest.Cardinality (Rest.Getter sid) (Rest.Getter mid)) -> Router (RunnableHandler m)
+routeNamed :: Rest.Resource m s sid mid aid
+           -> [Some1 (Rest.Router s)]
+           -> Rest.Endpoint sid mid aid
+           -> Router (RunnableHandler m)
 routeNamed resource@(Rest.Resource { Rest.list, Rest.statics }) subRouters h =
   case h of
     Left aid -> noRestPath >> hasMethod POST >> return (RunnableHandler id (statics aid))
     Right (Rest.Single getter) -> routeGetter getter resource subRouters
     Right (Rest.Many   getter) -> routeListGetter getter list
 
-routeUnnamed :: Rest.Resource m s sid mid aid -> [Some1 (Rest.Router s)] -> Rest.Cardinality (Rest.Id sid) (Rest.Id mid) -> Router (RunnableHandler m)
+routeUnnamed :: Rest.Resource m s sid mid aid
+             -> [Some1 (Rest.Router s)]
+             -> Rest.Cardinality (Rest.Id sid) (Rest.Id mid)
+             -> Router (RunnableHandler m)
 routeUnnamed resource@(Rest.Resource { Rest.list }) subRouters cardinality = popSegment >>= \seg ->
   case cardinality of
     Rest.Single sBy -> parseIdent sBy seg >>= \sid -> withSubresource sid resource subRouters
-    Rest.Many   mBy -> parseIdent mBy seg >>= \mid -> noRestPath >> hasMethod GET >> return (RunnableHandler id (mkListHandler (list mid)))
+    Rest.Many   mBy -> parseIdent mBy seg >>= \mid ->
+      do noRestPath
+         hasMethod GET
+         return (RunnableHandler id (mkListHandler (list mid)))
 
-routeGetter :: Rest.Getter sid -> Rest.Resource m s sid mid aid -> [Some1 (Rest.Router s)] -> Router (RunnableHandler m)
+routeGetter :: Rest.Getter sid
+            -> Rest.Resource m s sid mid aid
+            -> [Some1 (Rest.Router s)]
+            -> Router (RunnableHandler m)
 routeGetter getter resource subRouters =
   case getter of
     Rest.Singleton sid -> getOrDeep sid
-    Rest.By        sBy -> withSegment (multiPut resource sBy) $ \seg -> parseIdent sBy seg >>= getOrDeep
+    Rest.By        sBy -> withSegment (multiPut resource sBy) $ \seg ->
+                            parseIdent sBy seg >>= getOrDeep
   where
     getOrDeep sid = withSubresource sid resource subRouters
 
@@ -115,14 +149,25 @@ multiPut (Rest.Resource { Rest.update, Rest.enter }) sBy = hasMethod PUT >>
     Just updateH -> return (RunnableHandler id (mkMultiPutHandler sBy enter updateH))
     Nothing      -> apiError UnsupportedRoute
 
-routeListGetter :: Monad m => Rest.Getter mid -> (mid -> ListHandler m) -> Router (RunnableHandler m)
+routeListGetter :: Monad m
+                => Rest.Getter mid
+                -> (mid -> ListHandler m)
+                -> Router (RunnableHandler m)
 routeListGetter getter list = hasMethod GET >>
   case getter of
     Rest.Singleton mid -> noRestPath >> return (RunnableHandler id (mkListHandler (list mid)))
-    Rest.By        mBy -> popSegment >>= parseIdent mBy >>= \mid -> noRestPath  >> return (RunnableHandler id (mkListHandler (list mid)))
+    Rest.By        mBy ->
+      do seg <- popSegment
+         mid <- parseIdent mBy seg
+         noRestPath
+         return (RunnableHandler id (mkListHandler (list mid)))
 
-withSubresource :: sid -> Rest.Resource m s sid mid aid -> [Some1 (Rest.Router s)] -> Router (RunnableHandler m)
-withSubresource sid resource@(Rest.Resource { Rest.enter, Rest.selects, Rest.actions }) subRouters = withSegment (routeSingle sid resource) $ \seg ->
+withSubresource :: sid
+                -> Rest.Resource m s sid mid aid
+                -> [Some1 (Rest.Router s)]
+                -> Router (RunnableHandler m)
+withSubresource sid resource@(Rest.Resource { Rest.enter, Rest.selects, Rest.actions }) subRouters =
+  withSegment (routeSingle sid resource) $ \seg ->
   case lookup seg selects of
     Just select -> noRestPath >> hasMethod GET >> return (RunnableHandler (enter sid) select)
     Nothing -> case lookup seg actions of
@@ -135,7 +180,8 @@ withSubresource sid resource@(Rest.Resource { Rest.enter, Rest.selects, Rest.act
           Nothing -> apiError UnsupportedRoute
 
 routeSingle :: sid -> Rest.Resource m s sid mid aid -> Router (RunnableHandler m)
-routeSingle sid (Rest.Resource { Rest.enter, Rest.get, Rest.update, Rest.remove }) = ask >>= \method ->
+routeSingle sid (Rest.Resource { Rest.enter, Rest.get, Rest.update, Rest.remove }) =
+  ask >>= \method ->
   case method of
     GET    -> handleOrNotFound get
     PUT    -> handleOrNotFound update
@@ -203,7 +249,8 @@ guardMethod :: (MonadPlus m, MonadReader Method m) => Method -> m ()
 guardMethod method = ask >>= guard . (== method)
 
 mkListHandler :: Monad m => ListHandler m -> Handler m
-mkListHandler (GenHandler dict act sec) = GenHandler (addPar range . L.modify outputs listO $ dict) (mkListAction act) sec
+mkListHandler (GenHandler dict act sec) =
+  GenHandler (addPar range . L.modify outputs listO $ dict) (mkListAction act) sec
 
 mkMultiPutHandler :: Monad m => Rest.Id id -> (id -> Run s m) -> Handler s -> Handler m
 mkMultiPutHandler sBy run (GenHandler dict act sec) = GenHandler newDict newAct sec
@@ -218,7 +265,10 @@ mkMultiPutHandler sBy run (GenHandler dict act sec) = GenHandler newDict newAct 
               mapErrorT (run i) (act (Env hs ps v))
          return (StringMap (zipWith (\(k, _) b -> (k, eitherToStatus b)) vs bs))
 
-mkListAction :: Monad m => (Env h p i -> ErrorT (Reason e) m [a]) -> Env h ((Int, Int), p) i -> ErrorT (Reason e) m (List a)
+mkListAction :: Monad m
+            => (Env h p i -> ErrorT (Reason e) m [a])
+            -> Env h ((Int, Int), p) i
+            -> ErrorT (Reason e) m (List a)
 mkListAction act (Env h ((f, c), p) i) = do
   xs <- act (Env h p i)
   return (List f (min c (length xs)) (take c xs))
