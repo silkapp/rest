b89f72bbb788f738c38184f3963687b39634d562
Adam Bergmark <adam@bergmark.nl>
2014-01-14 17:52:34 +0100

    rest-gen: Use aeson

--- rest-gen/src/Rest/Gen/Base/JSON.hs
+++ rest-gen/src/Rest/Gen/Base/JSON.hs
@@ -1,20 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Rest.Gen.Base.JSON where
 
+import Data.Aeson ((.=))
 import Data.JSON.Schema
+import Data.Text (pack)
 import Rest.Gen.Base.JSON.Pretty
-import Text.JSON
 import Text.PrettyPrint.HughesPJ
+import qualified Data.Aeson as A
+import qualified Data.Vector as V
 
 showExample :: Schema -> String
 showExample = render . pp_value . showExample'
   where
-    showExample' (Choice [])     = JSNull -- Cannot create zero value
+    showExample' (Choice [])     = A.Null -- Cannot create zero value
     showExample' (Choice (x:_))  = showExample' x
-    showExample' (Object fs)     = JSObject $ toJSObject $ map (\f -> (key f, showExample' (content f))) fs
-    showExample' (Tuple vs)      = JSArray $ map showExample' vs
-    showExample' (Array l _ _ v) = JSArray $ replicate (l `max` 1) (showExample' v)
-    showExample' (Value _ _)     = JSString (toJSString "value")
-    showExample' Boolean         = JSBool True
-    showExample' (Number l _)    = JSRational False (toRational l)
-    showExample' Null            = JSNull
-
+    showExample' (Object fs)     = A.object $ map (\f -> pack (key f) .= showExample' (content f)) fs
+    showExample' (Tuple vs)      = A.Array $ V.fromList $ map showExample' vs
+    showExample' (Array l _ _ v) = A.Array $ V.fromList $ replicate (l `max` 1) (showExample' v)
+    showExample' (Value _ _)     = A.String "value"
+    showExample' Boolean         = A.Bool True
+    showExample' (Number l _)    = A.Number (fromIntegral l)
+    showExample' Null            = A.Null
--- rest-gen/src/Rest/Gen/Base/JSON/Pretty.hs
+++ rest-gen/src/Rest/Gen/Base/JSON/Pretty.hs
@@ -1,19 +1,24 @@
 module Rest.Gen.Base.JSON.Pretty where
 
-import Text.JSON.Types
-import Text.PrettyPrint.HughesPJ
-import Data.Ratio
+import Control.Arrow (first)
+import Data.Aeson.Types
 import Data.Char
+import Data.HashMap.Strict (HashMap)
+import Data.Text (Text, unpack)
 import Numeric
+import Text.PrettyPrint.HughesPJ hiding (first)
+import qualified Data.Attoparsec.Number as Attoparsec
+import qualified Data.HashMap.Strict as H
+import qualified Data.Vector as V
 
-pp_value         :: JSValue -> Doc
+pp_value         :: Value -> Doc
 pp_value v        = case v of
-    JSNull       -> pp_null
-    JSBool x     -> pp_boolean x
-    JSRational asf x -> pp_number asf x
-    JSString x   -> pp_js_string x
-    JSArray vs   -> pp_array vs
-    JSObject xs  -> pp_js_object xs
+    Null      -> pp_null
+    Bool x    -> pp_boolean x
+    Number x  -> pp_number x
+    String x  -> pp_js_string (unpack x)
+    Array vs  -> pp_array $ V.toList vs
+    Object xs -> pp_js_object xs
 
 pp_null          :: Doc
 pp_null           = text "null"
@@ -22,12 +27,11 @@ pp_boolean       :: Bool -> Doc
 pp_boolean True   = text "true"
 pp_boolean False  = text "false"
 
-pp_number        :: Bool -> Rational -> Doc
-pp_number _ x | denominator x == 1 = integer (numerator x)
-pp_number True x                   = float (fromRational x)
-pp_number _    x                   = double (fromRational x)
+pp_number        :: Attoparsec.Number -> Doc
+pp_number (Attoparsec.I n) = integer n
+pp_number (Attoparsec.D d) = double d
 
-pp_array         :: [JSValue] -> Doc
+pp_array         :: [Value] -> Doc
 pp_array xs       = vlist "[" "]" $ map pp_value xs
 
 vlist :: String -> String -> [Doc] -> Doc
@@ -47,12 +51,12 @@ pp_string x       = doubleQuotes $ hcat $ map pp_char x
                   | otherwise = cs
           where len = length cs
 
-pp_object        :: [(String,JSValue)] -> Doc
+pp_object        :: [(String,Value)] -> Doc
 pp_object xs      = vlist "{" "}" $ map pp_field xs
   where pp_field (k,v) = pp_string k <> colon <+> pp_value v
 
-pp_js_string     :: JSString -> Doc
-pp_js_string x    = pp_string (fromJSString x)
+pp_js_string     :: String -> Doc
+pp_js_string x    = pp_string x
 
-pp_js_object     :: JSObject JSValue -> Doc
-pp_js_object x    = pp_object (fromJSObject x)
+pp_js_object     :: HashMap Text Value -> Doc
+pp_js_object      = pp_object . map (first unpack) . H.toList
