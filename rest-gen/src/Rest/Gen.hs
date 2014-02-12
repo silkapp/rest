6c882a473653e83e18ce609006741dc41058e62b
Thomas van Noort <thomas@silk.co>
2014-02-12 16:33:51 +0100

    Rest-gen: Define generic generation of clients and documentation.
    Note that is a slightly improved generalisation. The -d and -h flags take additional arguments that are specific to that action, instead of separate flags.

---
+++ rest-gen/src/Rest/Gen.hs
@@ -0,0 +1,69 @@
+module Rest.Gen where
+
+import Data.Char
+import Data.Label
+import Data.Foldable
+import Data.Maybe
+import System.Cmd
+import System.Directory
+import System.Exit
+
+import Rest.Api (withVersion, Api, Some1 (..))
+
+import Rest.Gen.Config
+import Rest.Gen.Docs.Generate
+import Rest.Gen.JavaScript.Generate
+import Rest.Gen.Haskell.Generate hiding (apiVersion)
+import Rest.Gen.Ruby.Generate
+import Rest.Gen.Utils
+
+generate :: String -> Config -> Api m -> [(String, String)] -> IO ()
+generate name config api moduleRewrites =
+  withVersion (get apiVersion config) api (putStrLn "Could not find api version" >> exitFailure) $ \ver (Some1 r) ->
+     case get action config of
+       Just (MakeDocs root)        ->
+         do loc <- getTargetDir config "./docs"
+            setupTargetDir config loc
+            let context = DocsContext root ver (fromMaybe "./templates" (getSourceLocation config))
+            writeDocs context r loc
+            exitSuccess
+       Just MakeJS                 -> mkJsApi (moduleName ++ "Api") (get apiPrivate config) ver r >>= toTarget config
+       Just MakeRb                 -> mkRbApi (moduleName ++ "Api") (get apiPrivate config) ver r >>= toTarget config
+       Just (MakeHS cabalTemplate) ->
+         do loc <- getTargetDir config "./client"
+            setupTargetDir config loc
+            let context = HaskellContext ver loc cabalTemplate (packageName ++ "-client") (get apiPrivate config) moduleRewrites [moduleName, "Client"]
+            mkHsApi context r
+            exitSuccess
+       Nothing                     -> return ()
+  where
+    packageName = map toLower name
+    moduleName  = upFirst packageName
+
+getTargetDir :: Config -> String -> IO String
+getTargetDir config str =
+  case get target config of
+     Stream     -> putStrLn ("Cannot generate documentation to stdOut, generating to " ++ str) >> return str
+     Default    -> putStrLn ("Generating to " ++ str) >> return str
+     Location d -> putStrLn ("Generating to " ++ d) >> return d
+
+setupTargetDir :: Config -> String -> IO ()
+setupTargetDir config t =
+  do createDirectoryIfMissing True t
+     forM_ (getSourceLocation config) $ \s -> system $ "cp -rf " ++ s ++ " " ++ t
+
+toTarget :: Config -> String -> IO ()
+toTarget config code =
+  do let outf =
+          case get target config of
+              Stream         -> putStrLn
+              Default        -> putStrLn
+              Location l     -> writeFile l
+     outf code
+     exitSuccess
+
+getSourceLocation :: Config -> Maybe String
+getSourceLocation config =
+  case get source config of
+    Location s -> Just s
+    _          -> Nothing
---
+++ rest-gen/src/Rest/Gen/Config.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE TemplateHaskell, TypeOperators #-}
+module Rest.Gen.Config where
+
+import Prelude hiding ((.), id)
+
+import Control.Category
+import Data.Label
+import System.Console.GetOpt
+
+data Action   = MakeDocs String | MakeJS | MakeRb | MakeHS String
+data Location = Default | Stream | Location String
+
+data Config = Config
+  { _action     :: Maybe Action
+  , _source     :: Location
+  , _target     :: Location
+  , _apiVersion :: String
+  , _apiPrivate :: Bool
+  }
+
+$(mkLabels [''Config])
+
+defaultConfig :: Config
+defaultConfig = Config
+  { _action     = Nothing
+  , _source     = Default
+  , _target     = Default
+  , _apiVersion = "latest"
+  , _apiPrivate = True
+  }
+
+parseLocation :: String -> Location
+parseLocation "-" = Stream
+parseLocation s   = Location s
+
+options :: (a :-> Config) -> [OptDescr (a -> a)]
+options parent =
+  [ Option ['d'] ["documentation"] (ReqArg (set (action        . parent) . Just . MakeDocs) "URLROOT") "Generate API documentation, available under the provided URL root."
+  , Option ['j'] ["javascript"]    (NoArg  (set (action        . parent) (Just MakeJS))) "Generate Javascript bindings."
+  , Option ['r'] ["ruby"]          (NoArg  (set (action        . parent) (Just MakeRb))) "Generate Ruby bindings."
+  , Option ['h'] ["haskell"]       (ReqArg (set (action        . parent) . Just . MakeHS) "CABAL.ST") "Generate Haskell bindings, using the provided cabal template."
+  , Option ['s'] ["source"]        (ReqArg (set (source        . parent) . parseLocation) "LOCATION") "The location of additional sources."
+  , Option ['t'] ["target"]        (ReqArg (set (target        . parent) . parseLocation) "LOCATION") "The target location for generation."
+  , Option ['v'] ["version"]       (ReqArg (set (apiVersion    . parent)) "VERSION") "The version of the API under generation. Default latest."
+  , Option ['p'] ["hide-private"]  (NoArg  (set (apiPrivate    . parent) False)) "Generate API for the public, hiding private resources. Not default."
+  ]
