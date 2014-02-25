3fc469598e1f32fa91dbba7d5cda24c429ee6e5a
Thomas van Noort <thomas@silk.co>
2014-02-25 11:17:20 +0100

    Rest-gen: Layout.

--- rest-gen/src/Rest/Gen/Config.hs
+++ rest-gen/src/Rest/Gen/Config.hs
@@ -35,12 +35,12 @@ parseLocation s   = Location s
 
 options :: (a :-> Config) -> [OptDescr (a -> a)]
 options parent =
-  [ Option ['d'] ["documentation"] (ReqArg (set (action        . parent) . Just . MakeDocs) "URLROOT") "Generate API documentation, available under the provided URL root."
-  , Option ['j'] ["javascript"]    (NoArg  (set (action        . parent) (Just MakeJS))) "Generate Javascript bindings."
-  , Option ['r'] ["ruby"]          (NoArg  (set (action        . parent) (Just MakeRb))) "Generate Ruby bindings."
-  , Option ['h'] ["haskell"]       (NoArg  (set (action        . parent) (Just MakeHS))) "Generate Haskell bindings."
-  , Option ['s'] ["source"]        (ReqArg (set (source        . parent) . parseLocation) "LOCATION") "The location of additional sources."
-  , Option ['t'] ["target"]        (ReqArg (set (target        . parent) . parseLocation) "LOCATION") "The target location for generation."
-  , Option ['v'] ["version"]       (ReqArg (set (apiVersion    . parent)) "VERSION") "The version of the API under generation. Default latest."
-  , Option ['p'] ["hide-private"]  (NoArg  (set (apiPrivate    . parent) False)) "Generate API for the public, hiding private resources. Not default."
+  [ Option ['d'] ["documentation"] (ReqArg (set (action     . parent) . Just . MakeDocs) "URLROOT") "Generate API documentation, available under the provided URL root."
+  , Option ['j'] ["javascript"]    (NoArg  (set (action     . parent) (Just MakeJS))) "Generate Javascript bindings."
+  , Option ['r'] ["ruby"]          (NoArg  (set (action     . parent) (Just MakeRb))) "Generate Ruby bindings."
+  , Option ['h'] ["haskell"]       (NoArg  (set (action     . parent) (Just MakeHS))) "Generate Haskell bindings."
+  , Option ['s'] ["source"]        (ReqArg (set (source     . parent) . parseLocation) "LOCATION") "The location of additional sources."
+  , Option ['t'] ["target"]        (ReqArg (set (target     . parent) . parseLocation) "LOCATION") "The target location for generation."
+  , Option ['v'] ["version"]       (ReqArg (set (apiVersion . parent)) "VERSION") "The version of the API under generation. Default latest."
+  , Option ['p'] ["hide-private"]  (NoArg  (set (apiPrivate . parent) False)) "Generate API for the public, hiding private resources. Not default."
   ]
