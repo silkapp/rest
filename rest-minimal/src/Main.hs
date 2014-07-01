cdb1210d2658e16c887b1236f467dd70c15b818c
Adam Bergmark <adam@bergmark.nl>
2014-07-01 07:41:35 +0200

    Minimal example

---
+++ rest-minimal/src/Main.hs
@@ -0,0 +1,58 @@
+module Main where
+
+import Control.Monad.Reader
+import Data.IORef
+import Network.Wai.Handler.Warp
+import Rest
+import Rest.Api
+import Rest.Driver.Wai
+import Rest.Gen
+import Rest.Gen.Config
+import System.Environment
+import qualified Rest.Resource as R
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [] -> do
+      putStrLn "Starting warp server on http://localhost:3000"
+      st <- initialState
+      run 3000 $
+        apiToApplication (runApi st) api
+    "--gen":args' ->
+      withArgs args' $ do
+        config <- configFromArgs "minimal"
+        generate config "Minimal" api [] [] []
+    _ -> error "call with --gen or without arguments"
+
+api :: Api ApiState
+api = [(mkVersion 1 0 0, Some1 router)]
+
+router :: Router ApiState ApiState
+router =
+  root -/ route messageResource
+
+type ApiState = ReaderT (IORef [Int]) IO
+
+runApi :: IORef [Int] -> ApiState a -> IO a
+runApi s b = runReaderT b s
+
+initialState :: IO (IORef [Int])
+initialState = newIORef [0]
+
+messageResource :: Resource ApiState ApiState Int () Void
+messageResource = mkResourceId
+  { R.name   = "message"
+  , R.schema = withListing () $ named [("name", singleRead id)]
+  , R.list   = const getList
+  , R.create = Just setMessage
+  }
+
+getList :: ListHandler ApiState
+getList = mkListing (jsonO . someO) $ const (liftIO . readIORef =<< ask)
+
+setMessage :: Handler ApiState
+setMessage = mkInputHandler (jsonI . someI) $ \n -> do
+  ref <- ask
+  liftIO $ modifyIORef ref (n :)
