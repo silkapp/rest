914c3ddc958a3cb92b8ca647885ca4901a5e1021
Mikael Brockman <mikael@silk.co>
2014-09-02 17:39:53 +0200

    Whoops. Revert "JavaScript: fix SSL endpoints"
    This reverts commit b162457f7728e3fbea5d64eceaaae468f09a36b5.
    
    Whoops, I messed up.

--- rest-gen/files/Javascript/base.js
+++ rest-gen/files/Javascript/base.js
@@ -1,11 +1,10 @@
 var $apinamespace$ =
   function (url, secureUrl)
   {
-    var postfix          = '/v' + this.version + '/';
-    var contextUrl       = url + postfix;
-    var secureContextUrl = (secureUrl || url.replace(/^http:/, "https:")) + postfix;
-
-    $apinamespace$.setContext(this, contextUrl, secureContextUrl);
+    var postfix = '/v' + this.version + '/';
+    var sUrl = url + postfix;
+    $apinamespace$.setContext(this, sUrl);
+    this.secureContextUrl = (secureUrl || url.replace(/^http:/, "https:")) + postfix;
   };
 
 if (typeof module === "object" && module && typeof module.exports === "object")
@@ -35,6 +34,7 @@ $apinamespace$.addObject = function (obj1, obj2)
 };
 
 $apinamespace$.defaultAjaxOptions = {};
+$apinamespace$.defaultHeaders = {};
 
 function jQueryRequest (method, url, params, success, error, contentType, dataType, data, callOpts)
 {
@@ -48,12 +48,13 @@ function jQueryRequest (method, url, params, success, error, contentType, dataTy
     , dataType: dataType
     , xhrFields: { withCredentials: true }
     , data: data || []
+    , headers: $apinamespace$.defaultHeaders
     };
 
   $apinamespace$.addObject(callData, $apinamespace$.defaultAjaxOptions);
   $apinamespace$.addObject(callData, callOpts);
 
-  return $dollar$.ajax(callData);
+  return Q($dollar$.ajax(callData));
 }
 
 function nodeRequest (method, url, params, onSuccess, onError, contentType, dataType, data, callOpts)
@@ -72,6 +73,8 @@ function nodeRequest (method, url, params, onSuccess, onError, contentType, data
   else if (dataType === 'xml')
     headers.Accept = 'text/xml';
 
+  $apinamespace$.addObject(headers, $apinamespace$.defaultHeaders);
+
   var callData =
     { url     : url
     , qs      : allParams
@@ -84,16 +87,34 @@ function nodeRequest (method, url, params, onSuccess, onError, contentType, data
   $apinamespace$.addObject(callData, $apinamespace$.defaultAjaxOptions);
   $apinamespace$.addObject(callData, callOpts);
 
-  return require("request")(callData, callback);
-
-  function callback (error, message, response)
+  return require("q").Promise(function (resolve, reject)
   {
-    var parsedResponse = parse(response);
-    if (message.statusCode >= 200 && message.statusCode < 300)
-      onSuccess && onSuccess(parsedResponse, message);
-    else if (onError)
-      onError(error, parsedResponse, message);
-  }
+    require("request")(callData, callback);
+
+    function callback (error, message, body)
+    {
+      if (message.statusCode >= 200 && message.statusCode < 300)
+      {
+        var parsedResponse = parse(body);
+        onSuccess && onSuccess(parsedResponse, message);
+        resolve(parsedResponse)
+      }
+      else
+      {
+        if (!error)
+        {
+          error = new Error("HTTP request error");
+          error.statusCode = message.statusCode;
+          error.responseBody = body;
+        }
+
+        if (onError)
+          onError(error);
+
+        reject(error);
+      }
+    }
+  });
 
   function parse (response)
   {
@@ -104,16 +125,15 @@ function nodeRequest (method, url, params, onSuccess, onError, contentType, data
 }
 
 $apinamespace$.setContext =
-  function (obj, url, secureUrl)
+  function (obj, url)
   {
     obj.contextUrl = url;
-    obj.secureContextUrl = secureUrl;
     for (var fld in obj)
     {
       if (obj[fld] != undefined && obj[fld].apiObjectType != undefined && obj[fld].apiObjectType == 'resourceDir')
       {
-        var postfix = fld.replace(/([a-z0-9])([A-Z])/g, '$dollar$1-$dollar$2').toLowerCase() + '/';
-        $apinamespace$.setContext(obj[fld], url + postfix, secureUrl + postfix);
+        var newUrl = url + fld.replace(/([a-z0-9])([A-Z])/g, '$dollar$1-$dollar$2').toLowerCase() + '/'
+        $apinamespace$.setContext(obj[fld], newUrl);
       }
     }
   };
--- rest-gen/src/Rest/Gen/JavaScript.hs
+++ rest-gen/src/Rest/Gen/JavaScript.hs
@@ -47,10 +47,10 @@ mkRes ns node = mkStack $
 mkAccessorConstructor :: String -> ApiResource -> Code
 mkAccessorConstructor ns resource =
   let constrName = jsDir (cleanName (resName resource))
-  in functionDecl constrName ["url", "secureUrl"] $
+  in functionDecl constrName ["url"] $
        jsIf (code $ "this instanceof " ++ constrName)
-            (proc (ns ++ ".setContext") (code "this, url, secureUrl"))
-         <-> jsElse (ret $ call (constrName ++ ".access") (code "url, secureUrl"))
+            (proc (ns ++ ".setContext") (code "this, url"))
+         <-> jsElse (ret $ call (constrName ++ ".access") (code "url"))
 
 mkPreFuncs :: String -> ApiResource -> Code
 mkPreFuncs ns node =
@@ -77,9 +77,7 @@ mkAccessor ns node@(ApiAction _ _ ai) =
               ++ maybe "" (\i -> "' + encodeURIComponent(" ++ i ++ ") + '/") mIdent
       mIdent   = fmap (jsId . cleanName . description) $ ident ai
   in function fParams $
-      [ var "postfix" $ "'" ++ urlPart ++ "'"
-      , var "accessor" $ new "this" . code $  "this.contextUrl + postfix, "
-                                           ++ "this.secureContextUrl + postfix"
+      [ var "accessor" $ new "this" . code $ "this.contextUrl + '" ++ urlPart ++ "'"
       , "accessor.get" .=. mkFunction ns node
       , ret "accessor"
       ]
