cd204843b05db4d5706fcd2e75f6d46f2dcd21be
Adam Bergmark <adam@bergmark.nl>
2015-02-15 14:10:18 +0100

    Inline <!> in >|< to remove the need for 5 dependencies and to remove the Bind constraint

--- rest-core/src/Rest/Error.hs
+++ rest-core/src/Rest/Error.hs
@@ -17,8 +17,6 @@ module Rest.Error
 import Control.Applicative
 import Control.Monad.Error.Class
 import Control.Monad.Trans.Except
-import Data.Functor.Alt
-import Data.Functor.Bind (Bind)
 import Data.Semigroup
 
 import Rest.Types.Error
@@ -49,5 +47,11 @@ domainReason = CustomReason . DomainReason
 infixl 3 >|<
 -- | Combine two ExceptT computations yielding the last error if both fail.
 -- This prevents the need for a Semigroup or Monoid instance for the error type, which is necessary if using (<!>) or (<|>) respectively.
-(>|<) :: (Bind m, Applicative m, Monad m) => ExceptT e m a -> ExceptT e m a -> ExceptT e m a
+(>|<) :: (Applicative m, Monad m) => ExceptT e m a -> ExceptT e m a -> ExceptT e m a
 a >|< b = mapE getLast (mapE Last a <!> mapE Last b)
+  where
+    ExceptT m <!> ExceptT n = ExceptT $ do
+      v <- m
+      case v of
+        Left e -> fmap (either (Left . (<>) e) Right) n
+        Right x -> return (Right x)
