8286ef13ef22dde4430ce8a07ebedd6da2139036
Adam Bergmark <adam@bergmark.nl>
2015-06-30 11:59:09 +0200

    Undo changes to Happstack.Docs.apiDocsHandler, nothing changed there

--- rest-happstack/src/Rest/Driver/Happstack/Docs.hs
+++ rest-happstack/src/Rest/Driver/Happstack/Docs.hs
@@ -5,21 +5,15 @@ import Control.Monad
 import Control.Monad.Trans
 import Happstack.Server
 import Rest.Api
-
 import Rest.Gen.Base
 import Rest.Gen.Docs
 
 -- | Web interface for documentation
 apiDocsHandler :: (ServerMonad m, MonadPlus m, FilterMonad Response m, MonadIO m) => String -> String -> Api a -> m Response
-apiDocsHandler rootURL tmpls api = path $ \i -> withVersion i api mzero $ \v -> serve (mkCtx v i)
-  where
-    mkCtx :: Version -> String -> DocsContext
-    mkCtx v ct = DocsContext
-      { rootUrl         = rootURL ++ ct ++ "/"
-      , contextVersion  = v
-      , templates       = tmpls
-      }
-    serve ctx = serveDocs ctx . sortTree . noPrivate . (\(Some1 r) -> apiSubtrees r)
+apiDocsHandler rootURL tmpls api =
+  let mkCtx v ct = DocsContext (rootURL ++ ct ++ "/") v tmpls
+      serve ctx = serveDocs ctx . sortTree . noPrivate . (\(Some1 r) -> apiSubtrees r)
+  in path $ \i -> withVersion i api mzero $ \v -> serve (mkCtx v i)
 
 serveDocs :: (ServerMonad m, MonadPlus m, FilterMonad Response m, MonadIO m) => DocsContext -> ApiResource -> m Response
 serveDocs ctx tree =
